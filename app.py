4de1eb534a059cec0b428417c0f150966884fb2a
rtx3 <r@rtx3.com>
2016-07-28 13:33:07 +0800

    file upload

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from flask import Flask,render_template
-from flask.ext.bootstrap import Bootstrap
-from flask.ext.script import Manager
+from flask_bootstrap import Bootstrap
+from flask_script import Manager
 from datetime import datetime
-  
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def index():
+  return '<h1>test</h1>'
+
+if __name__ == '__main__':
+  app.run(debug=True)
---
+++ pyenv.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+ 
+ 
+source /fenghua/git/Salt-MWDS/env/bin/activate
+# echo $PWD
+
+if [ $1 == 'run' ]; then
+    echo "running"
+    /fenghua/git/Salt-MWDS/env/bin/python /fenghua/git/Salt-MWDS/app.py &
+    echo $!  > "/fenghua/git/Salt-MWDS/pid.tmp"
+    exit 0
+fi
+
+
+if [ $1 == 'stop' ]; then
+    echo "stopping"
+    tmp=`cat /fenghua/git/Salt-MWDS/pid.tmp`
+    kill -9 $tmp $[$tmp+1]
+    rm -rf /fenghua/git/Salt-MWDS/pid.tmp
+    exit 0
+fi
