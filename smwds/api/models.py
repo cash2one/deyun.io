05ea1f28fb478cd3403924f4c82c622afc3311a5
rtx3 <rtxiii@aliyun.com>
2016-08-19 15:19:37 +0800

    wrap salt api

--- smwds/api/models.py
+++ smwds/api/models.py
@@ -29,7 +29,7 @@ class Apidb(db.Model):
     master_api_url = Column(db.String(STRING_LEN), nullable=False, default="", info={'verbose_name' : u'主机API地址',})
     master_api_port  = Column(db.Integer, nullable=False, default=0, info={'verbose_name' : u'主机API端口',})
     username = Column(db.String(STRING_LEN),  nullable=False, default='salt')
-    _password = Column('password', db.String(200), nullable=False, default=generate_password_hash('sugar'))
+    password = Column(db.String(STRING_LEN),  nullable=False, default='sugar')
     #location = Column(db.String(STRING_LEN), nullable=False, default="")
     location_id = Column(UUIDType(binary=False), db.ForeignKey('location.id'), nullable=False, default="", info={'verbose_name' : u'提供商',})
     location = db.relationship('Location', backref = 'apis')
@@ -39,23 +39,13 @@ class Apidb(db.Model):
     update_at = Column(db.DateTime, info={'verbose_name' : u'更新时间',})
     operator = Column(UUIDType(binary=False), nullable=True, info={'verbose_name' : u'Master',})
     avatar = Column(db.String(STRING_LEN), nullable=False, default='')
+    token = Column(db.String(STRING_LEN), nullable=False, default='')
+    token_expire = Column(db.Float,nullable=False, default=0.0)
     
+    def ret_api(self):
+        return self.master_api_url + ":" + str(self.master_api_port)
 
-    def _get_password(self):
-        return self._password
 
-    def _set_password(self, password):
-        self._password = generate_password_hash(password)
-
-    def check_password(self, password):
-        if self.password is None:
-            return False
-        return check_password_hash(self.password, password)
-
-    # Hide password encryption by exposing password field only.
-    password = db.synonym('_password',
-                          descriptor=property(_get_password,
-                                              _set_password))
     
 
 class Location(db.Model):
--- smwds/api/views.py
+++ smwds/api/views.py
@@ -1,11 +1,13 @@
 
 # -*- coding: utf-8 -*-
 
+import time
 from flask import Blueprint, request, jsonify
 from flask_login import login_user, logout_user, current_user, login_required
 from flask_restful import Api, Resource
-from extensions import cache
+from extensions import cache, db
 from api.libpepper import Pepper
+from api.models import Apidb
 
 
 saltapi = Pepper()
@@ -14,12 +16,33 @@ api = Blueprint('api', __name__, url_prefix='/api/v1')
 api_wrap = Api(api, catch_all_404s=False)
 
 
-class saltapi(Resource):
+def getsaltapi(target):
+    sapi = Pepper(target.ret_api())
+    if ((time.time() - target.token_expire) >= 0.0):
+        ret = sapi.login(target.username, target.password, 'pam')
+        target.token = ret['token']
+        target.token_expire = ret['expire']
+        db.session.add(target)
+        db.session.commit()
+    else:
+        sapi.auth['token'] = target.token
+    return sapi
+
+
+class saltapi_get(Resource):
 
     @cache.memoize(timeout=60 * 60)
-    @login_required
-    def get(self, id):
-        return {'task': 'Say "Hello, World!"'}
+    #@login_required
+    def get(self, master_id, path):
+        target = Apidb.query.filter_by(id=master_id).first_or_404()
+        t_api = getsaltapi(target)
+        return t_api.req_get('/' + path)
 
 
-api_wrap.add_resource(saltapi, '/todos/<int:id>')
+
+#class saltapi_req(Resource):
+    
+
+
+
+api_wrap.add_resource(saltapi_get, '/<string:master_id>/<string:path>')
