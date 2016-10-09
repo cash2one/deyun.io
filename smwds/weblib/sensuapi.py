a0b20a995952abcfef5c9a1139e694398c7ed3d9
rtx3 <rtxiii@aliyun.com>
2016-10-09 17:06:43 +0800

    sensu api

--- smwds/weblib/sensuapi.py
+++ smwds/weblib/sensuapi.py
@@ -6,7 +6,16 @@ logger = logging.getLogger('api')
 
 
 class SensuAPI_Exception(Exception):
-    pass
+    def __init__(self, message, statuscode):
+    
+        # Call the base class constructor with the parameters it needs
+        super(SensuAPI_Exception, self).__init__(message)
+    
+        self.statuscode = statuscode
+
+
+
+
 
 
 class SensuAPI(object):
@@ -23,21 +32,17 @@ class SensuAPI(object):
           }
         try:
             resp = requests.get(**params)
-            if resp.status_code == 401:
-                raise SensuAPI_Exception(str(resp.status_code) +
-                                    ':Authentication denied')
-                return
-
-            if resp.status_code == 500:
-                raise SensuAPI_Exception(str(resp.status_code) + ':Server error.')
-                return
-
-            if resp.status_code == 404:
-                raise SensuAPI_Exception(str(resp.status_code) +
-                                    ' :This request returns nothing.')
-                return
         except SensuAPI_Exception as e:
+            if resp.status_code == 401:
+                raise SensuAPI_Exception('Authentication denied', statuscode=resp.status_code)
+            if resp.status_code == 500:
+                raise SensuAPI_Exception('server error', statuscode=resp.status_code)
+            if resp.status_code == 404:
+                raise SensuAPI_Exception('not found', statuscode=resp.status_code)
             logger.error('Error with request: {0}'.format(e))
             return
-        logger.info('Test Comlpeted with sucess.')
-        return resp.json()
+        if resp.status_code ==  200:
+            logger.info('Test Comlpeted with sucess.')
+            return True
+        else:
+            return False
