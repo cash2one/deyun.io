e9be9485f437cba64cbdacf0e90e67591e505302
rtx3 <rtxiii@aliyun.com>
2016-10-09 17:26:36 +0800

    sensu api get

--- smwds/weblib/sensuapi.py
+++ smwds/weblib/sensuapi.py
@@ -2,6 +2,16 @@ import requests
 import logging
 import os
 import datetime
+try:
+    from urllib.request import HTTPHandler, Request, urlopen, \
+        install_opener, build_opener
+    from urllib.error import HTTPError, URLError
+    import urllib.parse as urlparse
+except ImportError:
+    from urllib2 import HTTPHandler, Request, urlopen, install_opener, build_opener, \
+        HTTPError, URLError
+    import urlparse
+
 logger = logging.getLogger('api')
 
 
@@ -32,7 +42,7 @@ class SensuAPI(object):
           }
         try:
             resp = requests.get(**params)
-        except SensuAPI_Exception as e:
+        except Exception as e:
             if resp.status_code == 401:
                 raise SensuAPI_Exception('Authentication denied', statuscode=resp.status_code)
             if resp.status_code == 500:
@@ -46,3 +56,34 @@ class SensuAPI(object):
             return True
         else:
             return False
+
+    def get(self, path):
+        '''
+        A thin wrapper from get http method of sensuapi
+        '''       
+        headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'X-Requested-With': 'XMLHttpRequest',
+        }
+        params = {'url': self._construct_url(path),
+                  'headers': headers,
+                  'verify': False,
+                  }
+        try:
+            resp = requests.get(**params)           
+        except Exception as e:
+            if resp.status_code == 401:
+                raise SensuAPI_Exception('Authentication denied', statuscode=resp.status_code)
+            if resp.status_code == 500:
+                raise SensuAPI_Exception('server error', statuscode=resp.status_code)
+            if resp.status_code == 404:
+                raise SensuAPI_Exception('not found', statuscode=resp.status_code)
+            logger.error('Error with request: {0}'.format(e))
+            return
+        return resp.json()
+
+    def _construct_url(self, path):
+        
+        relative_path = path.lstrip('/')
+        return urlparse.urljoin(self.api_url, relative_path)
