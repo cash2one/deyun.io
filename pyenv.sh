7c9b6e92abff5efcd54c7b9a187bd034900951ad
rtx3 <rtxiii@aliyun.com>
2016-11-09 18:00:07 +0800

    env update

--- pyenv.sh
+++ pyenv.sh
@@ -6,7 +6,9 @@ source /fenghua/git/Salt-MWDS/env/bin/activate
 
 if [ $1 == 'run' ]; then
     echo "running"
-    /fenghua/git/Salt-MWDS/env/bin/python /fenghua/git/Salt-MWDS/smwds/app.py &
+#    /fenghua/git/Salt-MWDS/env/bin/python /fenghua/git/Salt-MWDS/smwds/app.py &
+    cd /fenghua/Git/Salt-MWDS/smwds
+    /fenghua/git/Salt-MWDS/env/bin/gunicorn -b 127.0.0.1:5000 --worker-class eventlet -w 1 "app:create_app()"
     echo $!  > "/fenghua/git/Salt-MWDS/pid.tmp"
     exit 0
 fi
