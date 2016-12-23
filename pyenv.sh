e2dc4f151731b0d6872e485814be370880bd4a37
rtx3 <rtxiii@aliyun.com>
2016-12-23 11:26:04 +0800

    depoly file

--- pyenv.sh
+++ pyenv.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+DEYUN_PATH="/fenghua/Git/Salt-MWDS"
+ENV_PATH="/fenghua/Git/Salt-MWDS/env"
  
- 
-source /fenghua/git/Salt-MWDS/env/bin/activate
+source $ENV_PATH/bin/activate
 # echo $PWD
 
 if [ $1 == 'run' ]; then
     echo "running"
 #    /fenghua/git/Salt-MWDS/env/bin/python /fenghua/git/Salt-MWDS/smwds/app.py &
-    cd /fenghua/Git/Salt-MWDS/smwds
-    /fenghua/git/Salt-MWDS/env/bin/gunicorn -b 127.0.0.1:5000 --worker-class eventlet -w 1 "app:create_app()"
-    echo $!  > "/fenghua/git/Salt-MWDS/pid.tmp"
+    cd $DEYUN_PATH/smwds
+    $DEYUN_PATH/env/bin/gunicorn -b 127.0.0.1:5000 --worker-class eventlet -w 1 "app:create_app()"
+    echo $!  > "$DEYUN_PATH/pid.tmp"
     exit 0
 fi
 
